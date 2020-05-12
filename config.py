284de166239c9db94950631fc89a9ee575eebf2d
Dan Njoroge <greatdaniels@gmail.com>
2020-05-12 11:25:24 +0300

    adding configurations file

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dannyboy:dannyboy@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class Prodconfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':Prodconfig
+}
